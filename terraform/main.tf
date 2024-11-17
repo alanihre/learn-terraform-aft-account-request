1c9f0a951918066a2146f99784564de29703d662
Alan Ihre Sherif <alih@MacBook-Pro-som-tillhor-Alan.local>
2024-11-17 23:54:22 +0100

    Create test-group Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,23 @@
-module "sandbox" {
+module "test-group" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    AccountEmail              = "alih+awsworkshoptestgroup@netlight.com"
+    AccountName               = "test-group"
+    ManagedOrganizationalUnit = "AFT"
+    SSOUserEmail              = "alih+awsworkshoptestgroup@netlight.com"
+    SSOUserFirstName          = "Test"
+    SSOUserLastName           = "Group"
   }
 
   account_tags = {
-    "Learn Tutorial" = "AFT"
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
   }
 
   custom_fields = {
-    group = "non-prod"
   }
 
-  account_customizations_name = "sandbox"
+  account_customizations_name = "test-group"
 }
